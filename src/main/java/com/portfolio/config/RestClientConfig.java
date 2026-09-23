4e898e1c145774064a95e809b8683aed54d9dfab
Manu <manusolerj@gmail.com>
2026-09-23 17:59:11 +0200

    docs: Update documentation comment for RestClientConfig class

--- src/main/java/com/portfolio/config/RestClientConfig.java
+++ src/main/java/com/portfolio/config/RestClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClient.Builder;
 
+/** Supplies the shared RestClient; timeouts come from `spring.http.clients.*`. */
 @Configuration
 public class RestClientConfig {
 
