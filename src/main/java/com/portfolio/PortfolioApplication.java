99e59d93df912405406783ceddcce3c952d4d46b
Manu <manusolerj@gmail.com>
2026-09-23 16:30:58 +0200

    chore: Add @EnableScheduling annotation to PortfolioApplication and import organization

--- src/main/java/com/portfolio/PortfolioApplication.java
+++ src/main/java/com/portfolio/PortfolioApplication.java
@@ -1,9 +1,12 @@
 package com.portfolio;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class PortfolioApplication {
